df7af0b2b33cd8b079c6dbc1255a06622354a366
Yauheni Mukhin <evgen3email@gmail.com>
2025-05-25 17:03:11 +0300

    cors added

--- app/project/settings/main.py
+++ app/project/settings/main.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework_swagger",
     "drf_spectacular",
+    "corsheaders",
     # first party
     "app.apps.users.apps.UsersConfig",
     "app.apps.transactions.apps.TransactionsConfig",
@@ -47,6 +48,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = "app.project.urls"
@@ -65,6 +67,10 @@ TEMPLATES = [
             ],
         },
     },
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5174",
 ]
 
 WSGI_APPLICATION = "app.project.wsgi.application"
