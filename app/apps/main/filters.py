e1c9d84292ece15287cdaee39ab22e9370970e40
Yauheni Mukhin <evgen3email@gmail.com>
2025-06-16 15:15:24 +0300

    add filter by date

--- app/apps/main/filters.py
+++ app/apps/main/filters.py
@@ -1,4 +1,4 @@
-from django_filters import CharFilter, FilterSet, UUIDFilter
+from django_filters import CharFilter, DateFromToRangeFilter, FilterSet, UUIDFilter
 
 from app.apps.main.models.transactions import Transaction
 
@@ -6,7 +6,8 @@ from app.apps.main.models.transactions import Transaction
 class TransactionFilter(FilterSet):
     type = CharFilter(field_name="type", lookup_expr="exact")
     category = UUIDFilter(field_name="category__id", lookup_expr="exact")
+    date = DateFromToRangeFilter()
 
     class Meta:
         model = Transaction
-        fields = ["type", "category"]
+        fields = ["type", "category", "date"]
